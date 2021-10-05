b1dc44ebad89ea522a89f304fe15f66679962c5e
LluisMas <16152037+LluisMas@users.noreply.github.com>
2021-10-05 23:17:49 +0200

    Save multiple file uploader/reader work

--- src/components/NewTaskForm/FileForm/index.js
+++ src/components/NewTaskForm/FileForm/index.js
@@ -1,26 +1,29 @@
-import * as React from 'react';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormLabel from '@mui/material/FormLabel';
+import React, { useState } from 'react';
 import Typography from "@mui/material/Typography";
 import Button from '@mui/material/Button';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 
 const FileForm = () => {
-    const [files, setFiles] = React.useState("");
+    const [files, setFile] = useState([]);
+    const MAX_ALLOWED_FILE = 10;
 
-    const handleChange = e => {
-        console.log(e.target);
-        const fileReader = new FileReader();
-        fileReader.readAsText(e.target.files[0], "UTF-8");
-        fileReader.onload = e => {
-            console.log("e.target.result", e.target.result);
-            setFiles(e.target.result);
+    async function uploadSingleFile(e) {
+        setFile([...files, URL.createObjectURL(e.target.files[0])]);
+
+        const reader = new FileReader();
+        reader.onload = async (x) => {
+            const text = (x.target.result);
+            console.log(text);
         };
-    };
+        reader.readAsText(e.target.files[0]);
+    }
+
+    function deleteFile(e) {
+        const s = files.filter((item, index) => index !== e);
+        setFile(s);
+        console.log(s);
+    }
+
     return (
         <>
             <Typography variant="h6" gutterBottom>
@@ -30,18 +33,31 @@ const FileForm = () => {
             <input
                 style={{ display: 'none' }}
                 id="raised-button-file"
-                multiple
                 type="file"
-                onChange={handleChange}
+                disabled={files.length >= MAX_ALLOWED_FILE}
+                multiple
+                onChange={uploadSingleFile}
             />
             <label htmlFor="raised-button-file">
                 <Button variant="outlined" component="span" endIcon={<FileUploadIcon/> }>
                     Upload
                 </Button>
             </label>
+            <span className="form-group preview">
+                {files.length > 0 &&
+                files.map((item, index) => {
+                    return (
+                        <div key={item}>
+                            <Typography> {item} </Typography>
+                            <button type="button" onClick={() => deleteFile(index)}>
+                                delete
+                            </button>
+                        </div>
+                    );
+                })}
+            </span>
 
             <br/>
-            {files}
         </>
     );
 };
